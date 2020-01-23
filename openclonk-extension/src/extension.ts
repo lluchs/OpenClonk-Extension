2c9b199c8dcf79819c84b4fa70227c78a7bf38d7
fmilke <fridjofmilke@googlemail.com>
2020-01-23 14:47:51 +0100

    added runScenario binding

--- openclonk-extension/src/extension.ts
+++ openclonk-extension/src/extension.ts
@@ -1,9 +1,12 @@
 import * as vscode from 'vscode';
 import { IC4groupProvider } from './Ifaces/IC4GroupProvider';
 import { C4GroupProvider } from './Impl/C4GroupProvider';
+import { IRunScenarioProvider } from './Ifaces/IRunScenarioProvider';
+import { RunScenarioProvider } from './Impl/RunScenarioProvider';
 
 export function activate(context: vscode.ExtensionContext) {
 	const provider: IC4groupProvider = new C4GroupProvider();
+	const runScenarioProvider: IRunScenarioProvider = new RunScenarioProvider();
 
 	context.subscriptions.push(vscode.commands.registerCommand('oc-ext.unpackC4g', ({ fsPath }) => {
 		provider.unpack(fsPath)
@@ -13,6 +16,10 @@ export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(vscode.commands.registerCommand('oc-ext.packC4g', ({ fsPath, ...args }) => {
 		provider.pack(fsPath)
 			.then(_ => vscode.commands.executeCommand("workbench.files.action.refreshFilesExplorer"));
+	}));
+
+	context.subscriptions.push(vscode.commands.registerCommand('oc-ext.runScenarioInEditor', ({ fsPath }) => {
+		runScenarioProvider.runScenarioInEditorMode(fsPath);
 	}));
 }
 
