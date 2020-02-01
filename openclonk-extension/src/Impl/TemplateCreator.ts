d2709b11ef9d9d035104ab9923360363d18d0558
fmilke <fridjofmilke@googlemail.com>
2020-02-01 20:49:39 +0100

    added template creator stup

---
+++ openclonk-extension/src/Impl/TemplateCreator.ts
@@ -0,0 +1,10 @@
+import { ITemplateCreator } from "../Ifaces/ITemplateCreator";
+
+export class TemplateCreator implements ITemplateCreator {
+    
+    createScenarioTemplate(): void {
+    }
+    
+    createObjectTemplate(): void {
+    }
+}
