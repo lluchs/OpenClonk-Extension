a1cc73215c0e0642cf504726f8f0c8a60a71f5a3
Lukas Werling <lukas@lwrl.de>
2020-03-08 23:03:49 +0100

    Add support for oclsp

--- openclonk-extension/src/extension.ts
+++ openclonk-extension/src/extension.ts
@@ -8,6 +8,46 @@ import { TemplateCreator } from './Impl/TemplateCreator';
 import { ITemplateSelection } from './Ifaces/ITemplateSelection';
 import { TemplateSelection } from './Impl/TemplateSelection';
 
+import {
+  LanguageClient,
+  LanguageClientOptions,
+  ServerOptions,
+  TransportKind
+} from 'vscode-languageclient';
+
+let client: LanguageClient;
+
+function activateLS(context: vscode.ExtensionContext, oclspPath: string) {
+  // If the extension is launched in debug mode then the debug server options are used
+  // Otherwise the run options are used
+  let serverOptions: ServerOptions = {
+    run: { command: oclspPath },
+    debug: { command: oclspPath },
+  };
+
+  // Options to control the language client
+  let clientOptions: LanguageClientOptions = {
+    // Register the server for OC c4script documents
+    documentSelector: [{ scheme: 'file', language: 'ocs' }],
+    synchronize: {
+	  // Notify the server about file changes to '.clientrc files contained in the workspace
+	  // TODO: what does this file do?
+      fileEvents: vscode.workspace.createFileSystemWatcher('**/.clientrc')
+    }
+  };
+
+  // Create the language client and start the client.
+  client = new LanguageClient(
+    'oclsp',
+    'OpenClonk Language Server',
+    serverOptions,
+    clientOptions
+  );
+
+  // Start the client. This will also launch the server
+  client.start();
+}
+
 export function activate(context: vscode.ExtensionContext) {
 	const provider: IC4groupProvider = new C4GroupProvider();
 	const runScenarioProvider: IRunScenarioProvider = new RunScenarioProvider();
@@ -37,6 +77,18 @@ export function activate(context: vscode.ExtensionContext) {
 			vscode.commands.executeCommand("workbench.files.action.refreshFilesExplorer");
 		}
 	}));
+
+  const oclsp = vscode.workspace.getConfiguration("oc-ext").get<string>("pathToOCLSPExecutable");
+  if (oclsp)
+    activateLS(context, oclsp);
+  else
+    vscode.window.showInformationMessage('Path to oclsp executable is not set, syntax checking is disabled. Please update your settings.');
 }
 
-export function deactivate() { }
+export function deactivate(): Thenable<void> | undefined {
+  if (!client) {
+    return undefined;
+  }
+  return client.stop();
+}
+
